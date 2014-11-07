61af89c2f5e2c479ab2a7b1fa92362653eba11dc
Bookis Smuin <bookis.smuin@gmail.com>
2014-11-07 10:19:48 -0800

    remove slash

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,8 @@ Dino.delete_all
 Egg.delete_all
 
 File.open("public/dino_names.txt").each_line do |name|
-  dino = Dino.create(name: Faker::Name.name, genus: name, height: rand(56), length: rand(120))
+  name = name.chomp
+  dino = Dino.create(name: Faker::Name.name, genus: name, height: name.length * 1.5, length: name.length * 2.5)
   dino.genus.scan(/[xyz]/).size.times do |n|
     dino.eggs.create(shell_color: Faker::Commerce.color, name: Faker::Name.name)
   end
